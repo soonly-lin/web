cc4748635e27502364c8f1cf692f35bae6cd62e3
soonly-lin <295616220@qq.com>
2018-06-26 18:44:16 +0800

    update

--- main.py
+++ main.py
@@ -1,79 +1,95 @@
 import re
-import requests
-import os, stat
+import os
 import urllib.request
+from urllib.parse import urlparse
 from urllib.parse import urljoin
-import  _thread
+import time
 
-siteMap = 'http://52.220.15.65:8080/opencms/'
-# siteMap = 'http://192.168.24.108:8180/opencms/en/'
-siteurl = re.findall(r'http[s]?:\/\/[^\/]+', siteMap)
-hostname = ''
-if siteurl.__len__() > 0:
-    hostname = siteurl[0]
+#siteMap = 'http://127.0.0.1:8080/opencms/sitemap/index.html'
+#siteMap = 'http://192.168.24.108:8180/opencms/en/'
+siteMap = 'http://52.220.15.65:8080/opencms/sitemap/index.html'
 
-
-def DownloadFile(fileUrl,html):
+def DownloadFile(pageUrl):
     try:
-        fileUrl = "/opencms"+fileUrl
-        pageUrl = urljoin(siteMap, fileUrl)
         print(pageUrl)
-        path = "."+os.path.dirname(fileUrl)
-        if not os.path.exists(path):
-             os.makedirs(path)
-        name = os.path.basename(fileUrl)
-        path =path+'/'+ name
-        print(name)
-        if (name =="index.html"):
-            f = open(path, 'w')
+        path = urlparse(pageUrl).path
+        paths =os.path.dirname(path)
+
+        if not os.path.exists("."+paths):
+            os.makedirs("."+paths)
+        if str(pageUrl).endswith(".html") == True:
+            relpath = os.path.relpath("./opencms/","./"+paths)
+            relpath = relpath.replace("\\","/")
+            htmls = getHtml(pageUrl)
+            html = re.sub(r'"\/opencms' , "\""+relpath , htmls)
+            html = re.sub(r'\'\/opencms' , "\'"+relpath , html)
+            html = re.sub(r'url\(\/opencms' , "url("+relpath , html)
+            html = re.sub(r'\/\"' , "/index.html\"" , html)
+            f = open("."+path, 'w',encoding='utf-8')
             f.write(html)
             f.close()
-            readPath(fileUrl)
-        # else:
-        #     f = open(path, 'wb')
-        #     f.write((urllib.request.urlopen(pageUrl)).read())
-        #     f.close()
+
+            urllist = getUrl(htmls)
+            for href in urllist:
+                href = getUrlPath(href)
+                pageUrl = urljoin(siteMap, href)
+                if (pageUrl not in urlSet and str(pageUrl).endswith(".html") == False):
+                    urlSet.add(pageUrl)
+                    DownloadFile(pageUrl)
+
+        else:
+            urllib.request.urlretrieve(pageUrl, filename="."+path)
 
     except Exception as e:
-        print("DownloadFile Exception：" + fileUrl)
+        print("DownloadFile Exception：" + pageUrl)
+
 
 def getHtml(url):
-    page = urllib.request.urlopen(url)
-    html = page.read()
-    html = html.decode('UTF-8')
-    return html
+    try:
+        page = urllib.request.urlopen(url)
+        html = page.read()
+        html = html.decode('UTF-8')
+        return html
+    except Exception as e:
+        print("getHtml Exception：" + url)
 
 def getUrl(html):
     reg = r'"\/opencms(.*?)"'
     urlre = re.compile(reg);
     urllist = re.findall(urlre, html)
+
+    regs = r'\'\/opencms(.*?)\''
+    urlres = re.compile(regs);
+    urllists = re.findall(urlres, html)
+    urllist.extend(urllists)
+
+    regs1 = r'url\(\/opencms(.*?)\)'
+    urlres1 = re.compile(regs1);
+    urllists1 = re.findall(urlres1, html)
+    urllist.extend(urllists1)
     return urllist
 
-def getUrl2(html):
-    reg = r'\'\/opencms(.*?)\''
-    urlre = re.compile(reg);
-    urllist = re.findall(urlre, html)
-    return urllist
+def getUrlPath(href):
+    href = re.sub('#[^#]+', '', href)
+    href = re.sub('\?[^\?]+', '', href)
+    if (str(href).endswith("/")):
+        href = href + "index.html"
+    return  '/opencms'+href
 
 def readPath(fileUrl):
     try:
-        fileUrl = urljoin(siteMap, fileUrl)
-        print("---------------------------" + fileUrl)
         html = getHtml(fileUrl)
         urllist = getUrl(html)
         for href in urllist:
-            href = re.sub('#[^#]+', '', href)
-            href = re.sub('\?[^\?]+', '', href)
-            if (str(href).endswith("/")):
-                href = href + "index.html"
-
-            if href not in hrefSet:
-                hrefSet.add(href)
-                DownloadFile(href,html)
-
+            href = getUrlPath(href)
+            pageUrl = urljoin(fileUrl, href)
+            if pageUrl not in urlSet:
+                urlSet.add(pageUrl)
+                DownloadFile(pageUrl)
     except Exception as e:
         print("Exception：" + fileUrl)
 
-srcSet = set()
-hrefSet = set()
+ticks = time.time()
+urlSet = set()
 readPath(siteMap)
+print(time.time()- ticks)
